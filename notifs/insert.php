02dcce8a01e24781b0b73710b56d4887da03091b
hamzaizend <63814867+hamzaizend@users.noreply.github.com>
2020-12-20 20:02:20 +0100

    Add files via upload

---
+++ notifs/insert.php
@@ -0,0 +1,23 @@
+<?php
+include("connect.php");
+
+if(isset($_POST["post"])){
+
+if(isset($_POST["subject"]))
+
+{
+
+
+
+$subject = mysqli_real_escape_string($con, $_POST["subject"]);
+
+$comment = mysqli_real_escape_string($con, $_POST["comment"]);
+
+$query = "INSERT INTO comments(comment_subject, comment_text)VALUES ('$subject', '$comment')";
+
+mysqli_query($con, $query);
+
+}
+}
+
+?>
